2a456b859c2117ebf60728eec423d90a6ae9681d
jery hardianto <jery@carigi.id>
2023-09-28 23:55:34 +0700

    feat: users register

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import {web} from "./application/web.js";
+import {logger} from "./application/logging.js";
+
+
+web.listen(300, () => {
+    logger.info("App start")
+})
---
+++ test/user.test.js
@@ -0,0 +1,23 @@
+import {web} from "../src/application/web.js";
+import supertest from "supertest";
+import {prismaClient} from "../src/application/database.js";
+
+describe('POST /api/users', function () {
+    it('should can register new user', async () => {
+        const result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: 'test',
+                password: 'rahasia',
+                name: 'test'
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.name).toBe("test");
+        expect(result.body.data.password).toBeUndefined();
+    });
+
+
+
+})
