69639dbae82104cd27e4c61bd0f7014a39524d0f
jery hardianto <jery@carigi.id>
2023-09-30 00:07:40 +0700

    fix: unit test user.test.js

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -2,7 +2,7 @@ import userService from "../service/user-service.js";
 
 const register = async (req, res, next) =>{
     try{
-        const result = userService.register(req.body);
+        const result = await userService.register(req.body);
         res.status(200).json({
            data: result
         });
